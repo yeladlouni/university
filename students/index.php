6e71e17a7e8d20c311595bf0d7c48dd8bff0293b
yeladlouni@gmail.com <med_2014/>
2020-01-08 15:52:43 +0100

    traitement des fichiers

--- students/index.php
+++ students/index.php
@@ -8,40 +8,38 @@
     <body onload="change_color()">
         <div class="content">
             <form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-           <table>
                <?php
-                    class Student {
-                        function Student($cne, $first_name, $last_name, $birth_date, $address) {
-                            $this->cne = $cne;
-                            $this->first_name = $first_name;
-                            $this->last_name = $last_name;
-                            $this->birth_date = $birth_date;
-                            $this->address = $address;
-                        }
-
-                        function check_age() {
-                            return -$this->birth_date + new DateTime() < 30 * 365; 
-                        }
-                    }  
+                    include 'student.php';
                     
-                    $students = array(
-                        new Student("1020029", "test", "test", "30/01/1990", "200 rue test"),
-                        new Student("1020029", "test", "test", "30/01/1990", "200 rue test"),
-                        new Student("1020029", "test", "test", "30/01/1990", "200 rue test"),
-                        new Student("1020029", "test", "test", "30/01/1990", "200 rue test")    
-                    );
+                    $fstudents = fopen('../data/students.csv', 'r') or die('Fichier inaccessible.');
+                   
+                    $students = array();
 
-                    if (empty($_POST['cne'])) {
-                        echo "<form><input ></form>";
-                        
-                    } else {
-                        echo "<table></table>";
-                        for ($i=0; $i < count($students); $i++) {
-                            
-                        }
+                    while(!feof($fstudents)) {
+                        $line = fgets($fstudents);
+                        if(!$line == "") {
+                            $list = explode(";", $line);
+                            $student = new Student($list[0], $list[1], $list[2], $list[3], $list[4], $list[5]);
+                            array_push($students, $student);
+                        }                       
                     }
+
+                    fclose($fstudents);
+
+                    echo "<table>";
+                    echo "<tr><td>CNE</td><td>Nom</td><td>Date de naissance
+                    </td><td>Addresse</td><td>Email</td><td>Tél.</td></tr>";
+                    for($i = 0; $i < count($students); $i++) {
+                        echo "<tr><td>".$students[$i]->cne."</td>".
+                        "<td>".$students[$i]->name."</td>".
+                        "<td>".$students[$i]->birthDate."</td>".
+                        "<td>".$students[$i]->address."</td>".
+                        "<td>".$students[$i]->mobile."</td>".
+                        "<td>".$students[$i]->email."</td></tr>";
+                    }
+                    echo "</table>";
                 ?>
-           </table>
+           
         </div>
     </body>
 </html>
